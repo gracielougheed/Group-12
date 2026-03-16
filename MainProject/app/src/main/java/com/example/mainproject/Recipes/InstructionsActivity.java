dc457d7863cf52994ca030559c1e10aa41e305af
gracie <lougheed@iastate.edu>
2026-03-16 18:35:38 +0000

    Starting layout for Recipe Details Page

--- MainProject/app/src/main/java/com/example/mainproject/Recipes/InstructionsActivity.java
+++ MainProject/app/src/main/java/com/example/mainproject/Recipes/InstructionsActivity.java
@@ -1,4 +1,5 @@
 package com.example.mainproject.Recipes;
 
 public class InstructionsActivity {
+
 }
--- MainProject/app/src/main/java/com/example/mainproject/Recipes/RecipeDetailsActivity.java
+++ MainProject/app/src/main/java/com/example/mainproject/Recipes/RecipeDetailsActivity.java
@@ -1,4 +1,6 @@
 package com.example.mainproject.Recipes;
 
 public class RecipeDetailsActivity {
+    //View Recipes List, View Individual Recipe; need files
+
 }
