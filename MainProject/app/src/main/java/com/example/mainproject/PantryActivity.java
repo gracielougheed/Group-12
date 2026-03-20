6e6a4d2de8b4365225bfc3321aa870ab560a7120
iljabsk <23375493@studentmail.ul.ie>
2026-03-20 17:40:53 +0000

    Firebase + ingredient window

--- MainProject/app/src/main/java/com/example/mainproject/PantryActivity.java
+++ MainProject/app/src/main/java/com/example/mainproject/PantryActivity.java
@@ -2,12 +2,30 @@ package com.example.mainproject;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PantryActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance(
+            "https://cookbook-d313f-default-rtdb.europe-west1.firebasedatabase.app/"
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Pantry");
         setContentView(R.layout.activity_pantry);
+
+        Button btnAdd = findViewById(R.id.btnAddIngredient);
+
+        btnAdd.setOnClickListener(v -> showAddIngredientDialog());
+    }
+
+    // Shows a window with input fields for adding a new ingredient
+    private void showAddIngredientDialog() {
+
     }
 }
